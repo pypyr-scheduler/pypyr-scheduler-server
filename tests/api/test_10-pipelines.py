30f0e50407d75bef852a858dec18db64b6df8021
David Zerrenner <dazer017@gmail.com>
2019-11-13 20:40:49 +0100

    path problem in test discovery

--- tests/api/test_10-pipelines.py
+++ tests/api/test_10-pipelines.py
@@ -1,15 +1,16 @@
 import pytest
 
-class TestPipelines():
-    endpoint = '/pipelines'
+
+class TestPipelines:
+    endpoint = "/pipelines"
 
     def test_fetch_all(self, testapp):
-        res = testapp.get(self.endpoint, headers={'Accept':'application/json'})
+        res = testapp.get(self.endpoint, headers={"Accept": "application/json"})
         data = res.json
         print("data:", data)
         assert res.status_int == 200
 
-    @pytest.mark.parametrize("method", ["POST", "PUT", "DELETE", ])
+    @pytest.mark.parametrize("method", ["POST", "PUT", "DELETE"])
     def test_http_method_not_allowed(self, testapp, method):
         res = testapp.request(self.endpoint, method=method, expect_errors=True)
-        assert res.status_int == 405  # method not allowed   
+        assert res.status_int == 405  # method not allowed
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,12 +2,14 @@
 derived from:
 https://github.com/hirose31/connexion-tiny-petstore/blob/master/tests/conftest.py
 """
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import pytest
 import webtest as wt
 
 # from connexion.mock import MockResolver
-
 from pyrsched.app import create_app
 
 
