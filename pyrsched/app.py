7aa8c471be16fc61c3d7407d8090230d73650e5e
dzerrenn <david.zerrenner@t-systems.com>
2019-11-16 10:36:30 +0100

    testing relative pipeline base_path

--- pyrsched/app.py
+++ pyrsched/app.py
@@ -13,13 +13,15 @@ def create_app(config_file, **api_extra_args):
     logging.info(f"trying to load {config_file}")
     config.read(config_file)
 
+    path = Path(os.path.abspath(__file__)).parent
+
     api_extra_args['validate_responses'] = False
 
     _app = connexion.FlaskApp(__name__, specification_dir='../conf/')
     _app.add_api('pypyr-scheduler.v1.yaml', **api_extra_args)
 
     _app.app.config.from_mapping({
-        'PYRSCHED_PIPELINES_BASE_PATH': "C:\Projects\pypyr-scheduler\pipelines_test",
+        'PYRSCHED_PIPELINES_BASE_PATH': str((path / ".." / "pipelines_test").resolve()),
         'ENV': 'development',
         'DEBUG': True,
     })
