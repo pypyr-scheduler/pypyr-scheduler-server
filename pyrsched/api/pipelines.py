ddd7d01a25f03f3af1723d18dbff2d9b80db7b8c
David Zerrenner <dazer017@gmail.com>
2019-11-16 22:07:49 +0100

    refactor: moved make_target_filename to utils

--- pyrsched/api/pipelines.py
+++ pyrsched/api/pipelines.py
@@ -4,16 +4,9 @@ from pathlib import Path
 from flask import current_app, send_file
 from werkzeug.utils import secure_filename
 
-import connexion
-
-from ..utils import empty_response
+from ..utils import empty_response, make_target_filename
 
 logger = logging.getLogger(__name__)
-
-def _make_target_filename(filename):
-    filename = secure_filename(filename)
-    base_path = current_app.config.get("PYRSCHED_PIPELINES_BASE_PATH", current_app.instance_path)
-    return (Path(base_path) / filename).resolve()
 
 
 def get_one(filename):
@@ -22,7 +15,7 @@ def get_one(filename):
     """
     logger.info(f'GET /pipelines/{filename}')
 
-    filename = _make_target_filename(filename)
+    filename = make_target_filename(filename)
 
     try:
         return send_file(filename, as_attachment=True, mimetype='application/x-yaml', attachment_filename='filename')
@@ -36,16 +29,16 @@ def get_all():
     """
     logger.info('GET /pipelines')
     base_path = Path(current_app.config.get("PYRSCHED_PIPELINES_BASE_PATH", current_app.instance_path))
-    return [{'filename':p.name} for p in base_path.iterdir()]
+    return [{'filename': p.name} for p in base_path.iterdir()]
 
 
-def _save_file(filename, content, success_status = 201):   
+def _save_file(filename, content, success_status=201):
     # make directory if it does not exist
-    target = _make_target_filename(filename)
+    target = make_target_filename(filename)
     if not target.parent.exists():
         target.parent.mkdir()
-    content.save(str(target))    
-    if success_status == 204:  # 204 NO CONTENT requires to send nothing, not even headers 
+    content.save(str(target))
+    if success_status == 204:  # 204 NO CONTENT requires to send nothing, not even headers
         return empty_response()
     return "OK, Pipeline was uploaded", success_status
 
@@ -54,7 +47,7 @@ def create(filename, body):
     logger.info(f'POST /pipelines/{filename}')
     if not filename == secure_filename(filename):
         logger.warning(f"Possible malicious filename detected: '{filename}'")
-        return "Not Acceptable, possible malicious content detected", 406       
+        return "Not Acceptable, possible malicious content detected", 406
     return _save_file(filename, body)
 
 
@@ -62,19 +55,19 @@ def update(filename, body):
     logger.info(f'PUT /pipelines/{filename}')
     if not filename == secure_filename(filename):
         logger.warning(f"Possible malicious filename detected: '{filename}'")
-        return "Not Acceptable, possible malicious content detected", 406     
-    target_file = Path(_make_target_filename(filename))
+        return "Not Acceptable, possible malicious content detected", 406
+    target_file = make_target_filename(filename)
     if not target_file.exists():
         return "Pipeline not found", 404
     return _save_file(filename, body, success_status=204)
 
 
 def delete(filename):
-    logger.info(f'DELETE /pipelines/{filename}')    
+    logger.info(f'DELETE /pipelines/{filename}')
     if not filename == secure_filename(filename):
         logger.warning(f"Possible malicious filename detected: '{filename}'")
-        return "Not Acceptable, possible malicious content detected", 406 
-    target_file = Path(_make_target_filename(filename))   
+        return "Not Acceptable, possible malicious content detected", 406
+    target_file = make_target_filename(filename)
     try:
         target_file.unlink()
         return empty_response()
--- pyrsched/utils.py
+++ pyrsched/utils.py
@@ -1,7 +1,17 @@
-from flask import make_response
+from flask import make_response, current_app
+from werkzeug.utils import secure_filename
+
+from pathlib import Path
+
 
 def empty_response():
     res = make_response('', 204)
     res.headers = {} 
     res.content_length = 0
     return res
+
+
+def make_target_filename(filename):
+    filename = secure_filename(filename)
+    base_path = current_app.config.get("PYRSCHED_PIPELINES_BASE_PATH", current_app.instance_path)
+    return (Path(base_path) / filename).resolve()
