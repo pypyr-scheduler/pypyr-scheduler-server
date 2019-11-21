a2fb2b7a1b17b83980d7a6d6a8a5599566c8106b
dzerrenner <dazer017@gmail.com>
2019-11-21 12:29:00 +0100

    make sure the log dir exists

--- pyrsched/app.py
+++ pyrsched/app.py
@@ -1,4 +1,5 @@
 import logging
+from pathlib import Path
 
 import connexion
 from flask_ini import FlaskIni
@@ -19,10 +20,15 @@ def create_app(config_file, **api_extra_args):
 
     with _app.app.app_context():
         logging.info(f'trying to load {config_file}')
+        _app.app.json_encoder = JobEncoder
+
         _app.app.iniconfig = FlaskIni()
         _app.app.iniconfig.read(config_file)
 
-        _app.app.json_encoder = JobEncoder
+        # make sure the log path exists
+        log_path = Path(_app.app.iniconfig.get('pipelines', 'log_path')).resolve()
+        if not log_path.exists():
+            log_path.mkdir()
 
         _app.app.scheduler = BackgroundScheduler()
         _app.app.scheduler.start()
